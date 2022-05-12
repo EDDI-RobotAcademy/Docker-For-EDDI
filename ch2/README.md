803f625346550a8bd89ce72a3266efd76a9cfe91
silenc3502 <gcccompil3r@gmail.com>
2022-05-13 03:49:49 +0900

    How to use Docker with Dockerfile

---
+++ ch2/README.md
@@ -0,0 +1,5 @@
+## How to test Dockerfile
+
+```make
+1. docker-test.sh 실행
+```
--- ch2/docker-test.sh
+++ ch2/docker-test.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 
-#
+# 모든 이미지 삭제가 필요한 경우
+#docker rmi -f $(docker images -q)
+
+# 컨테이너 전부 삭제
+docker rm $(docker ps -qa)
+
+# Dockerfile 기반으로 이미지 생성
 docker build -t test/cowsay-dockerfile .
 
-#
+# 상태 저장
+docker commit cowsay test/cowsayimage
+
+# 생성한 이미지의 실행 파일 구동
 docker run test/cowsay-dockerfile /usr/games/cowsay "Moo"
 
-#
+# 상태 확인
 docker ps -a
